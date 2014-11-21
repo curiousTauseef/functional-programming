75105665b716510e895b76b37890fd50e9c4f937
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-21 17:38:22 +0100

    Adds more examples of using zip function

--- list_comprehensions.hs
+++ list_comprehensions.hs
@@ -1,13 +1,14 @@
 -- list comprehensions
 flatten xss = [x | xs <- xss, x <- xs]
 factors n = [x | x <- [1..n], n `mod` x == 0]
-prime n = factors n == [1, n]
-sum100 = sum [x ^ 2 | x <- [1..100]]
+isPrime n = factors n == [1, n]
 replicateValue n a = [a | _ <- [1..n]]
 pyths n = [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+isPerfect n == sum(init(factors n)) == n
 perfects n = [x | x <- [1..n], isPerfect x]
-             where isPerfect num = sum(init(factors num)) == num
+find k t = [v | (k', v) <- t, k == k']
 -- zip function
 pairs xs = zip xs (tail xs)
 sorted xs = and [x <= y | (x,y) <- pairs xs]
-positions x xs = [i | (x', i) <- zip xs [0..length xs -1], x == x']
+positions x xs = find x (zip xs [0..length xs - 1])
+scalarproduct xs ys = sum [x * y | (x, y) <- xs `zip` ys]
