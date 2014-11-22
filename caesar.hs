ba20abac610c1584936fe5c088b337a5f2a37004
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-22 15:27:42 +0100

    Caesar shift code now works with uppercase letters

--- caesar.hs
+++ caesar.hs
@@ -1,2 +1,11 @@
 -- Caesar cipher
+import Data.Char
 
+encode n xs = [shift n x | x <- xs]
+shift n c
+    | isLower c = int2letter ((letter2int c + n) `mod` 26)
+    | isUpper c = toUpper (int2letter ((letter2int (toLower c) + n) `mod` 26))
+    | otherwise = c
+
+letter2int c = ord c - ord 'a'
+int2letter n = chr (ord 'a' + n)
