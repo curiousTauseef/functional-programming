00a1c9f14f3b4fdf1454dddb5ef600a4e8d11348
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-26 02:34:29 +0100

    Adds a recurrent implementation of mergesort

--- recurrence.hs
+++ recurrence.hs
@@ -19,3 +19,10 @@ merge [] ys = ys
 merge xs [] = xs
 merge (x : xs) (y: ys) 
     = if x <= y then x : merge xs (y : ys) else y : merge (x : xs) ys
+
+halve xs = splitAt (length xs `div` 2) xs
+
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort left) (msort right)
+    where (left, right) = halve xs
