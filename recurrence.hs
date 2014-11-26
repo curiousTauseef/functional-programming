3f9824ea348ede4279c1d8f860345354ff1a6bf9
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-26 02:24:36 +0100

    Adds more examples of recurrent functions

--- recurrence.hs
+++ recurrence.hs
@@ -1,2 +1,21 @@
 allTrue [] = True
 allTrue (b:bs) = b && allTrue bs
+
+flatten [] = []
+flatten (xs : xss) = xs ++ flatten xss
+
+replicateValue 0 _ = []
+replicateValue n x = x : replicateValue (n - 1) x
+
+selectElem 0 (x : _) = x
+selectElem n (_ : xs) = selectElem (n - 1) xs
+
+isElem _ [] = False
+isElem x (y : ys)
+    | x == y = True
+    | otherwise = isElem x ys
+
+merge [] ys = ys
+merge xs [] = xs
+merge (x : xs) (y: ys) 
+    = if x <= y then x : merge xs (y : ys) else y : merge (x : xs) ys
